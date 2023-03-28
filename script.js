06d41364c09ac3920cf98d1ef46eb89296626b98
kcode-gg <floriangayk95@gmail.com>
2023-03-28 11:11:15 +0200

    Working as intended - Add One Round Function - Add game function to play 5 games - Track and display score to user - Show Winner, Loser or Tie message

--- script.js
+++ script.js
@@ -2,20 +2,20 @@
 
 
 //Player Input 
-let playerSelection = prompt("Please choose one of the following: Rock, Scissor, Paper");
+let playerSelection; //prompt("Please choose one of the following: Rock, Scissor, Paper");
 
 
 // Check for player input and repeat prompt if not given one of the options
-while (playerSelection.toLowerCase() !== "rock" && playerSelection.toLowerCase() !== "paper" && playerSelection.toLowerCase() !== "scissor"){
-        playerSelection = prompt("Please choose one of the following: Rock, Scissor, Paper");
-    }
+//while (playerSelection.toLowerCase() !== "rock" && playerSelection.toLowerCase() !== "paper" && playerSelection.toLowerCase() !== "scissor"){
+//        playerSelection = prompt("Please choose one of the following: Rock, Scissor, Paper");
+//    }
 
 
 
- alert("Good Luck!")
+
 
 //convert input to lowercase so it matches the computer choice in casing
-playerSelection = playerSelection.toLowerCase();
+//playerSelection = playerSelection.toLowerCase();
 
 
 
@@ -39,34 +39,76 @@ function getComputerChoice(){
    
 }
 
+
+let playerScore = 0;
+let computerScore = 0;
+
+
 //one Game Round - still unsure with parameters / arguments of functions
 function oneRound (strOutput, playerSelection){
     if (strOutput === "rock" && playerSelection === "scissor"){
         alert(`You Lost. ${strOutput} beats ${playerSelection}!`);
+        computerScore++;
     } else if (strOutput === "scissor" && playerSelection === "paper"){
         alert(`You Lost. ${strOutput} beats ${playerSelection}!`);
+        computerScore++;
     } else if (strOutput === "paper" && playerSelection === "rock"){
         alert(`You Lost. ${strOutput} beats ${playerSelection}!`);
+        computerScore++;
     } else if (strOutput === "rock" && playerSelection === "paper"){
         alert(`Congratulations! ${playerSelection} beats ${strOutput}!`);
+       playerScore++;
     } else if (strOutput === "scissor" && playerSelection === "rock"){
         alert(`Congratulations! ${playerSelection} beats ${strOutput}!`);
+        playerScore++;
     } else if (strOutput === "paper" && playerSelection === "scissor"){
         alert(`Congratulations! ${playerSelection} beats ${strOutput}!`);
+        playerScore++;
     } else {
         alert(`It's a draw. You both chose ${playerSelection}!`)
     }
         
-    
-    
+   
     
    
     }
+   
+    
 
+//game();
+//getComputerChoice();
+//oneRound(strOutput, playerSelection);
+//console.log(strOutput);
 
+function game(){
+    getComputerChoice();
+    
+    playerSelection = prompt("Please choose one of the following: Rock, Scissor, Paper");
 
-getComputerChoice();
-oneRound(strOutput, playerSelection);
-console.log(strOutput);
+    // Check for player input and repeat prompt if not given one of the options
+    while (playerSelection.toLowerCase() !== "rock" && playerSelection.toLowerCase() !== "paper" && playerSelection.toLowerCase() !== "scissor"){
+        playerSelection = prompt("Please choose one of the following: Rock, Scissor, Paper");
+    }
 
+    //convert input to lowercase so it matches the computer choice in casing
+    playerSelection = playerSelection.toLowerCase();
+   
+    //calling one round with computer and play selection
+    oneRound(strOutput, playerSelection);
+    console.log(computerScore);
+    console.log(playerScore);
+    alert(`Your score: ${playerScore} \n Computer score: ${computerScore}`);
+}
 
+for (let i = 0; i < 5; i++){
+    game();
+   
+}
+
+if (computerScore > playerScore){
+    alert("You lost vs a computer LOL! \n \n Refresh to play again.");
+} else if (computerScore < playerScore){
+    alert("You won! Congratulations you're the GOAT! \n \n Refresh to play again.")
+} else {
+    alert("It's a tie. \n \n Refresh to play again.")
+}
